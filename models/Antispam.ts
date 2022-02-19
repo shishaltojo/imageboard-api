ab5970077118e1b2b99b664392bf1503590875f9
Alejandro <alex.aleman.public@gmail.com>
2022-02-18 22:02:34 -0600

    Replicating Tinyboard Database structure

---
+++ models/Antispam.ts
@@ -0,0 +1,38 @@
+import { DataTypes } from 'sequelize';
+
+const Antispam = (sequelize:any) => {
+	const Antispam = sequelize.define('antispam', {
+		board: {
+			type: DataTypes.STRING(58),
+			charset: 'utf8',
+			allowNull: false,
+		},
+		thread: {
+			type: DataTypes.INTEGER,
+			defaultValue: null,
+		},
+		hash: {
+			type: DataTypes.STRING(40),
+			collate: 'ascii_bin',
+			allowNull: false,
+		},
+		created: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		expires: {
+			type: DataTypes.INTEGER,
+			defaultValue: null,
+		},
+		passed: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		}
+	}, {
+
+	});
+
+	return Antispam;
+};
+
+export default Antispam;
